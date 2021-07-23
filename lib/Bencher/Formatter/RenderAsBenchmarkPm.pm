baef1d8d68bcf016ca917a6a55983a6a86fc1cde
perlancar (on pc-home) <perlancar@gmail.com>
2021-07-23 20:04:39 +0700

    Doh, typo

--- lib/Bencher/Formatter/RenderAsBenchmarkPm.pm
+++ lib/Bencher/Formatter/RenderAsBenchmarkPm.pm
@@ -41,7 +41,7 @@ sub render_result {
         my $it = $items->[$i];
         push @rows, [
             $it->{_succinct_name},
-            (defined($it->{rate}) ? "$it->{rate}/s" : sprintf("%.1f/s", 1/$it->{rate})),
+            (defined($it->{rate}) ? "$it->{rate}/s" : sprintf("%.1f/s", 1/$it->{time})),
         ];
         for my $j (0..$#{$items}) {
             my $pct;
