e687f86db510720ae6f89da98a93f75ca6ef7a0e
perlancar (on pc-home) <perlancar@gmail.com>
2021-07-31 08:17:07 +0700

    [bugfix] When rendering result as Benchmark.pm result, fix calculating rate from time (time is in ms not s)

--- lib/Bencher/Formatter/RenderAsBenchmarkPm.pm
+++ lib/Bencher/Formatter/RenderAsBenchmarkPm.pm
@@ -41,7 +41,7 @@ sub render_result {
         my $it = $items->[$i];
         push @rows, [
             $it->{_succinct_name},
-            (defined($it->{rate}) ? "$it->{rate}/s" : sprintf("%.1f/s", 1/$it->{time})),
+            (defined($it->{rate}) ? "$it->{rate}/s" : sprintf("%.1f/s", 1000/$it->{time})),
         ];
         for my $j (0..$#{$items}) {
             my $pct;
