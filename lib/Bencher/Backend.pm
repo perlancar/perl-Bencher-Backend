cf57b6bd8fbd17d6f9fbf94b328b58bd89747ceb
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2022-02-08 08:19:55 +0700

    [bugfix] Fix missing schema in format_result()'s Rinci metadata

--- lib/Bencher/Backend.pm
+++ lib/Bencher/Backend.pm
@@ -2085,6 +2085,7 @@ $SPEC{format_result} = {
             'x.name.is_plural' => 1,
             'x.name.singular' => 'exclude_formatter',
             summary => 'Exclude Formatters specification',
+            schema => ['array*', of=>'str*'],
         },
         options => {
             'x.name.is_plural' => 1,
